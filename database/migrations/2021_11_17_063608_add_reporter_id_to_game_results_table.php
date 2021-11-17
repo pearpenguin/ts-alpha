842fe72707b4ea97f80a54b4407887426d662891
Kenley Cheung <cheung.kenley@gmail.com>
2021-11-17 19:37:46 +1300

    Add reporter_id to game_results table

---
+++ database/migrations/2021_11_17_063608_add_reporter_id_to_game_results_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddReporterIdToGameResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('game_results', function (Blueprint $table) {
+            $table->unsignedBigInteger('reporter_id');
+            $table->foreign('reporter_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('game_results', function (Blueprint $table) {
+            $table->dropForeign(['reporter_id']);
+            $table->dropColumn('reporter_id');
+        });
+    }
+}
